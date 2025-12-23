aae487719bff58ad5c2712c14334b98d768b5f35
chetracoding <hongchetra12@gmail.com>
2025-12-23 19:30:28 +0700

    feat(report): export as pdf

---
+++ src/controllers/export-controller.js
@@ -0,0 +1,37 @@
+import puppeteer from 'puppeteer'
+
+export async function exportPDF(req, res, next) {
+  try {
+    const { html, filename } = req.body
+
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
+
+    const page = await browser.newPage()
+    await page.setContent(html, { waitUntil: 'networkidle0' })
+
+    const pdf = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+      margin: {
+        top: '0.3in',
+        right: '0.3in',
+        bottom: '0.3in',
+        left: '0.3in',
+      },
+    })
+
+    await browser.close()
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': `attachment; filename="${filename}.pdf"`,
+      'Content-Length': pdf.length,
+    })
+
+    res.end(pdf)
+  } catch (error) {
+    next(error)
+  }
+}
