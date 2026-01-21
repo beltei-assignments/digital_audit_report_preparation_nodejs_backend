bb764f06fb2e957bd025adc3390d4649ddaceff9
chetracoding <hongchetra12@gmail.com>
2026-01-22 00:23:34 +0700

    chore(app): debug redis

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -3,9 +3,12 @@ import { config } from '../../boot/index.js'
 
 const client = redis.createClient({
   url: config.redisURL,
+  socket: {
+    connectTimeout: 10000,
+  },
 })
 
-client.on("error", err => console.error("Redis Error:", err));
+client.on('error', (err) => console.error('Redis Error:', err))
 
 // Connect to Redis immediately
 const connectRedis = async () => {
